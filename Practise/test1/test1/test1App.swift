665b63d6457f919014861a830e76580ee082866d
jaosn.li <jason.lee.ticktick@gmail.com>
2022-04-04 11:05:29 -0400

    reddit

---
+++ Practise/test1/test1/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  test1
+//
+//  Created by jianli on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
